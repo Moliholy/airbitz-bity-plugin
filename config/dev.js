3ff13454ecb1b5e4c28d8598cd871be50122c454
Alejandro Avilés <omegak@gmail.com>
2017-04-12 10:38:36 +0200

    Use correct OAuth client_id for plugin in dev

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,5 @@ const defaultCfg = require('./default');
 
 module.exports = Object.assign({}, defaultCfg, {
   HOST: 'https://dev.sbex.ch/',
-  CLIENT_ID: 'o9FNeKNxVvYnJm6ANamRmJezkvEeXnRrYowp6ihB'
+  CLIENT_ID: '5zwT7JOqGWqD4gEWqdfEAXa7lelyzZk4aiuF03mM'
 });
